e8ec6156166f35b5e759cc2ab5fc3d2c91f7b706
rezaputraa <mrezaputraa@gmail.com>
2022-09-16 18:05:18 +0700

    Modified tab spaces from 2 to 4.

--- js/emailValidation.js
+++ js/emailValidation.js
@@ -1,10 +1,10 @@
 function emailValidation(inputText) {
-  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (inputText.value.match(mailformat)) {
-    alert("Email berhasil dimasukan!");
-    return true;
-  } else {
-    alert("Email gagal dimasukan!");
-    return false;
-  }
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputText.value.match(mailformat)) {
+        alert("Email berhasil dimasukan!");
+        return true;
+    } else {
+        alert("Email gagal dimasukan!");
+        return false;
+    }
 }
